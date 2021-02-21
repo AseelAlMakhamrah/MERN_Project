434bd775b8e6715fbe0ddce2e82f728f6419ded9
Montaser Alfrookh <montaser.alfrookh91@gmail.com>
2021-02-21 16:25:17 +0200

    make pages and parts folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
-import Register from "./components/Register";
-import Login from "./components/Login";
-import Home from "./Home";
+import Register from "./components/pages/Register";
+import Login from "./components/pages/Login";
+import Home from "./components/pages/Home";
+import CreatePost from "./components/parts/CreatePost"
+
+
 
 function App() {
   return (
@@ -12,6 +15,7 @@ function App() {
         <Route exact path="/register" component={Register} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/" component={Home} />
+        <Route exact path="/create-post" component={CreatePost} />
       </Switch>
     </BrowserRouter>
   );
